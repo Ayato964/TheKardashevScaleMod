5dae27050a1c96aa3585f045bad73805288ab5b8
Ayato964 <chtk20063@g.nihon-u.ac.jp>
2023-05-22 04:05:02 +0900

    Version.Classic.0.1.0 Block TEST
    New Block
    * Steel_Block

--- src/main/java/net/ayato/tksmod/tabs/CreativeTabs.java
+++ src/main/java/net/ayato/tksmod/tabs/CreativeTabs.java
@@ -16,9 +16,12 @@ import java.awt.*;
 @Mod.EventBusSubscriber(modid = TheKardashevScaleMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CreativeTabs {
     public static CreativeModeTab ITEM_TAB;
+    public static CreativeModeTab BLOCK_TAB;
     @SubscribeEvent
     public static void registerCreativeTabs(CreativeModeTabEvent.Register event){
         ITEM_TAB = event.registerCreativeModeTab(new ResourceLocation(TheKardashevScaleMod.MODID, "item_tab"),
                 builder -> builder.icon(()-> new ItemStack(TKSItems.WOOD_GEAR.get())).title(Component.translatable("tkstabs.item_tab")));
+        BLOCK_TAB = event.registerCreativeModeTab(new ResourceLocation(TheKardashevScaleMod.MODID, "block_tab"),
+                builder -> builder.icon(()-> new ItemStack(TKSItems.STEEL_INGOT.get())).title(Component.translatable("tkstabs.block_tab")));
     }
 }
